d7c8a219db5a70c6ef8e4eedd319fc0eb4699935
luotiansheng <redgogh@vip.qq.com>
2026-01-16 16:46:12 +0800

    md5: add md5 module

---
+++ tools/include/r9k/ssl/md5.h
@@ -0,0 +1,39 @@
+/*
+-* SPDX-License-Identifier: MIT
+ * Copyright (c) 2025
+ *
+ * example:
+ *
+ * int main() {
+ *      const char *msg = "hello";
+ *      uint8_t digest[16];
+ *      int i;
+ *
+ *      struct md5_ctx md5;
+ *
+ *      md5_init(&md5);
+ *      md5_update(&md5, msg, strlen(msg));
+ *      md5_final(&md5, digest);
+ *
+ *      for (i = 0; i < 16; i++)
+ *          printf("%02x", digest[i]);
+ *      putchar('\n');
+ * }
+ *
+ */
+#ifndef MD5_H_
+#define MD5_H_
+
+#include <stdlib.h>
+
+typedef struct {
+        uint32_t a, b, c, d;
+        uint64_t len;
+        uint8_t  buf[64];
+} MD5_CTX;
+
+void md5_init(MD5_CTX *ctx);
+void md5_update(MD5_CTX *ctx, const void *data, size_t len);
+void md5_final(MD5_CTX *ctx, uint8_t out[16]);
+
+#endif /* MD5_H_ */
--- tools/list.c
+++ tools/list.c
@@ -1,5 +1,5 @@
 /*
-* SPDX-License-Identifier: MIT
+-* SPDX-License-Identifier: MIT
  * Copyright (c) 2025
  */
 #include <r9k/list.h>
