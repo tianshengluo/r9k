e7aa81e8dc4bbc0c9dc85f6a4a51b95659255f79
luotiansheng <redgogh@vip.qq.com>
2026-01-20 15:40:17 +0800

    gateway: use kqueue

--- gateway/socket.c
+++ gateway/socket.c
@@ -10,7 +10,7 @@
 #include <errno.h>
 #include <fcntl.h>
 
-int socket_start(int port, int max_listen)
+int socket_start(int port)
 {
         int fd;
         int opt = 1;
@@ -19,7 +19,7 @@ int socket_start(int port, int max_listen)
         fd = socket(AF_INET, SOCK_STREAM, 0);
         if (fd < 0) {
                 perror("socket create failed");
-                return SOCK_ERR_CREATE;
+                return -1;
         }
 
         setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
@@ -31,13 +31,13 @@ int socket_start(int port, int max_listen)
         if (bind(fd, (struct sockaddr *) &addr, sizeof(struct sockaddr_in)) < 0) {
                 perror("bind failed");
                 close(fd);
-                return SOCK_ERR_BIND;
+                return -1;
         }
 
-        if (listen(fd, max_listen) < 0) {
+        if (listen(fd, 65535) < 0) {
                 perror("listen failed");
                 close(fd);
-                return SOCK_ERR_LISTEN;
+                return -1;
         }
 
         return fd;
@@ -47,12 +47,12 @@ int socket_accept(int fd, struct sockaddr_in *addr)
 {
         int cli;
 
-        for (;;) {
+        while (1) {
                 if (addr) {
-                        socklen_t len = sizeof(addr);
+                        socklen_t len = sizeof(struct sockaddr_in);
 
                         cli = accept(fd,
-                                     (struct sockaddr *) addr,
+                                     (struct sockaddr *) &addr,
                                      &len);
                 } else {
                         cli = accept(fd, NULL, NULL);
@@ -61,89 +61,22 @@ int socket_accept(int fd, struct sockaddr_in *addr)
                 if (cli >= 0)
                         return cli;
 
-                if (errno == EINTR)
-                        continue;
+                RETRY_ONCE_ENTR();
 
+                if (errno == EAGAIN || errno == EWOULDBLOCK)
+                        return -1;
+
+                perror("socket_accept() failed");
                 return -1;
         }
-}
-
-ssize_t socket_read(int fd, void *ptr, size_t size, int flags)
-{
-        size_t off = 0;
-
-        while (off < size) {
-                ssize_t n = recv(fd, (char *) ptr + off, size - off, flags);
-
-                if (n == 0)
-                        return off ? (ssize_t) off : 0;
-
-                if (n < 0) {
-                        if (errno == EINTR)
-                                continue;
-                        if (errno == EAGAIN || errno == EWOULDBLOCK)
-                                return off ? (ssize_t) off : 0;
-                        return -1;
-                }
-
-                off += n;
-        }
-
-        return (ssize_t) off;
-}
-
-ssize_t socket_write(int fd, const void *ptr, size_t size, int flags)
-{
-        size_t off = 0;
-
-        while (off < size) {
-                ssize_t n = send(fd, (char *) ptr + off, size - off, flags);
-
-                if (n <= 0) {
-                        if (errno == EINTR)
-                                continue;
-                        if (errno == EAGAIN || errno == EWOULDBLOCK)
-                                return off ? (ssize_t) off : 0;
-                        return -1;
-                }
-
-                off += n;
-        }
-
-        return (ssize_t) off;
 }
 
 int set_nonblock(int fd)
 {
         int flags = fcntl(fd, F_GETFL, 0);
+
         if (flags < 0)
                 return -1;
+
         return fcntl(fd, F_SETFL, flags | O_NONBLOCK);
-}
-
-sockconn_t *socket_conn_alloc(size_t stagbuf_size)
-{
-        sockconn_t *conn;
-
-        conn = calloc(1, sizeof(conn));
-        if (!conn)
-                return NULL;
-
-        conn->stagbuf = calloc(1, stagbuf_size);
-        if (!conn->stagbuf) {
-                free(conn);
-                return NULL;
-        }
-
-        conn->size = stagbuf_size;
-
-        return conn;
-}
-
-void socket_conn_free(sockconn_t *conn)
-{
-        if (conn) {
-                free(conn->stagbuf);
-                free(conn);
-        }
 }
--- gateway/socket.h
+++ gateway/socket.h
@@ -6,34 +6,17 @@
 #define SOCKET_H_
 
 #include <arpa/inet.h>
+#include <sys/fcntl.h>
+#include <errno.h>
 
-#define SOCK_SERVER 0
-#define SOCK_CLIENT 1
+#define RETRY_ONCE_ENTR()   \
+        if (errno == EINTR) \
+                continue
 
-typedef enum
-{
-        SOCK_ERR_CREATE = -1,
-        SOCK_ERR_BIND,
-        SOCK_ERR_LISTEN,
-        SOCK_ERR_ACCEPT,
-} sockerr_t;
+#define is_eagain() (errno == EAGAIN || errno == EWOULDBLOCK)
 
-typedef struct {
-        int    fd;
-        size_t size;
-        size_t off;
-        char  *stagbuf;
-} sockconn_t;
-
-int socket_start(int port, int max_listen);
+int socket_start(int port);
 int socket_accept(int fd, struct sockaddr_in *addr);
-
-ssize_t socket_read(int fd, void *ptr, size_t size, int flags);
-ssize_t socket_write(int fd, const void *ptr, size_t size, int flags);
-
 int set_nonblock(int fd);
-
-sockconn_t *socket_conn_alloc(size_t stagbuf_size);
-void socket_conn_free(sockconn_t *conn);
 
 #endif /* SOCKET_H_ */
